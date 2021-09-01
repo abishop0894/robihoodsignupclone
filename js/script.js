67f0186c0b5251fb924666d76a25595051f05576
abishop0894 <82795351+abishop0894@users.noreply.github.com>
2021-09-01 13:42:39 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function blue () {
+    document.getElementById("threehundred").style.visibility = "visible";
+}
+
+function blueDown () {
+    document.getElementById("threehundred").style.visibility = "hidden";
+}
+
+function red () {
+    document.getElementById("sixhundred").style.visibility = "visible";
+}
+
+function redDown () {
+    document.getElementById("sixhundred").style.visibility = "hidden";  
+}
